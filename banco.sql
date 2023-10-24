65502a63e1247f98e9c6b11be62e6c10aa4a80e8
MAUMOREIRA <mauriciomoreirajr2006@gmail.com>
2023-10-24 10:46:44 -0300

    atualizadooo

--- banco.sql
+++ banco.sql
@@ -9,7 +9,6 @@ CREATE TABLE usuario (
     senha varchar(60) NOT NULL,
     data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     ativo tinyint NOT NULL DEFAULT '0',
-    adm tinyint NOT NULL DEFAULT '0',
     PRIMARY KEY (id)
     
 );
@@ -37,4 +36,17 @@ CREATE TABLE comentarios(
     PRIMARY KEY (id),
     CONSTRAINT fk_avaliacao_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id),
     CONSTRAINT fk_avaliacao_post FOREIGN KEY (post_id) REFERENCES post (id)
+);
+
+CREATE TABLE videos(
+    id int NOT NULL AUTO_INCREMENT,
+    titulo varchar(255) NOT NULL,
+    texto text NOT NULL,
+    usuario_id int NOT NULL,
+    data_criacao datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data_postagem datetime NOT NULL,
+    PRIMARY KEY (id),
+    KEY fk_post_usuario_idx (usuario_id),
+    CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+
 );
